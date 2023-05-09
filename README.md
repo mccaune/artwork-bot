06dff7ec394baabdece4c9c4b64cd38333322a7e
Mairis Caune <73192871+mccaune@users.noreply.github.com>
2023-05-09 22:09:25 +0300

    adding 1 more museum API

--- README.md
+++ README.md
@@ -1,33 +1,57 @@
-# artwork-bot
+# Artwork Bot
 
-This Python script utilizes the Tweepy library and the Metropolitan Museum of Art's API to periodically tweet information about a random piece of art, including its title, classification, artist, and date. The bot downloads and tweets an image of the artwork, then deletes the image file to save space. The script logs relevant events, such as composing and posting a tweet, or any errors that may occur.
+Artwork Bot is a Python application that posts images and information about artworks from the Metropolitan Museum of Art and the Rijksmuseum on Twitter at regular intervals.
+
+## Features
+
+- Fetches random artwork data from the Metropolitan Museum of Art and the Rijksmuseum using their public APIs
+- Composes a tweet containing information about the artwork and a link to the artwork page
+- Posts the tweet along with the artwork image to Twitter
+- Automatically resizes images larger than 5MB to comply with Twitter's image size limits
+- Logs application activity, including posted tweets and errors
 
 ## Requirements
 
-To run this script, you'll need the following:
-
 - Python 3.6 or higher
-- Tweepy library
-- Python-dotenv library
-- Requests library
-- A Twitter Developer account with API access
+- tweepy
+- requests
+- python-dotenv
+- Pillow
 
 ## Installation
 
 1. Clone the repository:
-git clone https://github.com/your-username/your-repo-name.git
 
-2. Install the required libraries:
-pip install tweepy python-dotenv requests
+git clone https://github.com/yourusername/artwork_bot.git
+cd artwork_bot
 
-3. Create a `.env` file in the project directory and add your Twitter API credentials:
-CONSUMER_KEY=your_consumer_key
-CONSUMER_SECRET=your_consumer_secret
-ACCESS_TOKEN=your_access_token
-ACCESS_TOKEN_SECRET=your_access_token_secret
+2. Install the required packages:
+
+pip install -r requirements.txt
+
+3. Create a `.env` file in the project root directory with the following contents, replacing the placeholder values with your own API keys and access tokens:
+
+CONSUMER_KEY=your_twitter_consumer_key
+CONSUMER_SECRET=your_twitter_consumer_secret
+ACCESS_TOKEN=your_twitter_access_token
+ACCESS_TOKEN_SECRET=your_twitter_access_token_secret
+RIJKSMUSEUM_API_KEY=your_rijksmuseum_api_key
+
+4. Run the application:
+
+python artwork_bot.py
 
 ## Usage
 
-1. Run the script:
+After setting up the application, it will automatically fetch random artwork data from the Metropolitan Museum of Art and the Rijksmuseum, compose tweets, and post them to Twitter every 10 hours.
 
-The bot will begin posting tweets with information about random artworks from the Metropolitan Museum of Art every 10 hours. The time interval between tweets can be adjusted by modifying the `post_interval` variable in the script.
+To change the posting interval, modify the `post_interval` variable in `artwork_bot.py`. The interval is specified in seconds.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- [The Metropolitan Museum of Art Collection API](https://metmuseum.github.io/)
+- [The Rijksmuseum API](https://www.rijksmuseum.nl/en/api)
--- twitter_art_bot.py
+++ twitter_art_bot.py
@@ -5,6 +5,7 @@ import os
 import random
 import logging
 from dotenv import load_dotenv
+from PIL import Image
 
 # Load environment variables
 load_dotenv()
@@ -18,6 +19,9 @@ consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
+# Set up Rijksmuseum parameters
+RIJKSMUSEUM_API_KEY = os.getenv("RIJKSMUSEUM_API_KEY")
+
 # Authenticate with the Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -25,6 +29,7 @@ api = tweepy.API(auth)
 
 # Set the time interval for posting tweets (in seconds)
 post_interval = 36000 # 10 hours
+
 
 def get_metropolitan_artwork_data():
     # API endpoint and parameters
@@ -63,6 +68,38 @@ def get_metropolitan_artwork_data():
         # Wait for a shorter time before trying to get the artwork data again
         time.sleep(50)
 
+def get_rijksmuseum_artwork_data():
+    endpoint = "https://www.rijksmuseum.nl/api/en/collection"
+    random_page = random.randint(1, 10000)  # Get a random page number between 1 and 100
+    params = {
+        "key": RIJKSMUSEUM_API_KEY,
+        "format": "json",
+        "imgonly": True,
+        "ps": 1,
+        "toppieces": False,
+        "p": random_page  # Add the random page parameter
+    }
+
+    try:
+        response = requests.get(endpoint, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        artwork = random.choice(data['artObjects'])
+
+        primary_image_url = artwork['webImage']['url']
+        object_url = artwork['links']['web']
+        object_name = artwork['longTitle']
+        object_title = artwork['title']
+        object_date = artwork['id']
+        object_artist = artwork['principalOrFirstMaker']
+
+        return primary_image_url, object_url, object_name, object_title, object_artist, object_date
+
+    except (requests.exceptions.RequestException, ValueError) as error:
+        logging.error("Error occurred while getting artwork data: %s", error)
+        time.sleep(50)
+
 def make_tweet(artwork_data):
     # Unpack the tuple
     primary_image_url, object_url, object_name, object_title, object_artist, object_date = artwork_data
@@ -94,9 +131,28 @@ def make_tweet(artwork_data):
     print(tweet)
     return tweet
 
+def make_rijksmuseum_tweet(artwork_data):
+    primary_image_url, object_url, object_name, object_title, object_artist, object_date = artwork_data
+
+    tweet = f"Check out this artwork from the Rijksmuseum! It's titled '{object_title}' ({object_url})"
+
+    if len(tweet) > 280:
+        tweet = tweet[:276] + "..."
+        logging.warning("Tweet was truncated to 280 characters")
+
+    logging.info(f"Composed tweet: {tweet}")
+    print(tweet)
+    return tweet
+
 
 def post_tweet(tweet):
     try:
+        # Check if the image size is within Twitter's allowed limit (5MB)
+        if os.path.getsize("artwork.jpg") > 1 * 1024 * 1024:
+            print("Error occurred while posting tweet: The image is still larger than 5MB after resizing")
+            logging.error("Error occurred while posting tweet: The image is still larger than 5MB after resizing")
+            return
+
         # Post the tweet to Twitter
         api.update_status_with_media(filename="artwork.jpg", status=tweet)
 
@@ -109,28 +165,63 @@ def post_tweet(tweet):
         # Wait for a shorter time before trying to post the next tweet
         time.sleep(60)
 
-# Loop indefinitely, posting a tweet every 10 hours
-while True:
-    # Get data about a random artwork from the Metropolitan Museum of Art API
-    artwork_data = get_metropolitan_artwork_data()
+def get_random_museum_artwork_data():
+    museum_function, make_tweet_function = random.choice(museum_api_functions)
+    return museum_function(), make_tweet_function
 
-    # Download the primary image for the artwork, if it exists
-    image_url = artwork_data[0]
-    if image_url:
-        try:
-            image_data = requests.get(image_url).content
-        except (requests.exceptions.RequestException, ValueError) as error:
-            logging.error("Error occurred while downloading image: %s", error)
 
-            # Wait for a shorter time before trying to download the image again
-            time.sleep(50)
-            continue
-
+def download_image(image_url):
+    try:
+        image_data = requests.get(image_url).content
         with open("artwork.jpg", "wb") as f:
             f.write(image_data)
 
+        # Resize the image if it's larger than 5MB
+        resize_image("artwork.jpg")
+
+        # Check if the image size is within Twitter's allowed limit (5MB)
+        if os.path.getsize("artwork.jpg") > 5 * 1024 * 1024:
+            print("Error occurred while downloading image: The image is still larger than 5MB after resizing")
+            logging.error("Error occurred while downloading image: The image is still larger than 5MB after resizing")
+            return False
+
+    except (requests.exceptions.RequestException, ValueError) as error:
+        logging.error("Error occurred while downloading image: %s", error)
+        time.sleep(50)
+        return False
+    return True
+
+
+def resize_image(image_path, max_size=1 * 1024 * 1024):
+    img = Image.open(image_path)
+    img_size = os.path.getsize(image_path)
+
+    if img_size > max_size:
+        scaling_factor = (max_size / img_size) ** 0.5
+        new_width = int(img.width * scaling_factor)
+        new_height = int(img.height * scaling_factor)
+        img = img.resize((new_width, new_height), Image.LANCZOS)
+        img.save(image_path, optimize=True, quality=85)
+
+    img.close()
+
+
+# List of museum API functions and their corresponding make_tweet functions
+museum_api_functions = [
+    (get_metropolitan_artwork_data, make_tweet),
+    (get_rijksmuseum_artwork_data, make_rijksmuseum_tweet)
+]
+
+# Loop indefinitely, posting a tweet every 10 hours
+while True:
+    # Get data about a random artwork from a random museum API
+    artwork_data, make_tweet_function = get_random_museum_artwork_data()
+
+    # Download the primary image for the artwork, if it exists
+    image_url = artwork_data[0]
+    if image_url and download_image(image_url):
         # Compose a tweet from the artwork data
-        tweet = make_tweet(artwork_data)
+        tweet = make_tweet_function(artwork_data)
 
         # Post the tweet to Twitter
         post_tweet(tweet)
@@ -140,7 +231,7 @@ while True:
             os.remove("artwork.jpg")
             logging.info(f"Image successfully deleted!")
         except OSError as e:
-            logging.error("Error deleting image file:: %s", error)
+            logging.error("Error deleting image file:: %s", e)
 
     # Wait for the specified interval before posting the next tweet
     time.sleep(post_interval)
